d2d97491a9652a393b61e824eb77d79a36436518
iamfip <linuxossysdev@gmail.com>
2021-08-20 11:44:46 +0600

    Staged

--- baap-er-bank/banking.js
+++ baap-er-bank/banking.js
@@ -1,7 +1,39 @@
 document.getElementById('deposit-button').addEventListener('click',function(){
     const depositInput= document.getElementById("deposit-input");
-    const depositAmount= depositInput.value;
-    console.log(parseFloat(depositAmount));
-    //const depositTotal=document.getElementById("deposit-total")
+    const depositAmount= parseFloat (depositInput.value);
+    // const depositTotal=parseFloat(depositAmount);
+    console.log(depositAmount);
+
+    // GEt depostTotal amont
+
+   const depositTotalText= document.getElementById("deposit-total");
+   const depositTotal= parseFloat(depositTotalText.innerText);
+   depositTotalText.innerText= depositTotal + depositAmount;
+
+   // update Balance
+    const balaceTotal=document.getElementById("balance-total");
+    const TotalBalance=parseFloat(balaceTotal.innerText);
+    balaceTotal.innerText=TotalBalance + depositAmount;
+
+    depositInput.value = '';
+})
+
+
+///// WithDraw Section
+
+document.getElementById("withdraw-button").addEventListener('click',function(){
+    const withdrowInput=document.getElementById("withdraw-input");
+    const withdrawAmount= parseFloat(withdrowInput.value);
+    // console.log(withdrawAmount);
+    const withdrawTotalText= document.getElementById("withdraw-total");
+    const withdrawTotal=parseFloat(withdrawTotalText.innerText);
+
+    withdrawTotalText.innerText=withdrawTotal+withdrawAmount;
+
+    const balaceTotal=document.getElementById("balance-total");
+    const TotalBalance=parseFloat(balaceTotal.innerText);
+    balaceTotal.innerText=TotalBalance - withdrawTotal;
+
+    withdrowInput.value = '';
 
 })
