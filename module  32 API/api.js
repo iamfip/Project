4ff4863851f4d24dee3156313f4d1b85eaa5da11
Farhan <linuxossysdev@gmail.com>
2021-08-30 22:16:39 +0600

    updated

---
+++ module  32 API/api.js
@@ -0,0 +1,27 @@
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()).
+    then(date  => displayusers(date));
+}
+
+/* function loadPosts(){
+    fetch('hhttps://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json()).
+    then(json => console.log(json));
+}  */
+
+function displayusers(data){
+    const ul = document.getElementById("users");
+    for ( const user of data){
+        console.log(user.name);
+        const li= document.createElement('li');
+        li.innerText=user.name, user.;
+        ul.appendChild(li);
+    }
+}
