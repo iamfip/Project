c4a35e575bf874cff7e5b7fd14bbb7e90aef9fef
Farhan <linuxossysdev@gmail.com>
2021-08-31 08:34:15 +0600

    Kanye Quote site update

---
+++ module 33 API examples/js/kanye.js
@@ -0,0 +1,14 @@
+ const loadquotes = () => {
+     fetch('https://api.kanye.rest/')
+     .then(response => response.json())
+     .then(data => displayquote(data));
+ }
+
+ const displayquote= quote =>{
+     const getElement=document.getElementById('quote');
+     quote.i
+     getElement.innerText=quote.quote;
+     
+     
+     
+ }
