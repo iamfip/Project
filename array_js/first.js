833a7a83ba1a1d478f0b8260fb9383e5019788a6
Farhan <linuxossysdev@gmail.com>
2021-09-04 19:01:21 +0600

    update

---
+++ array_js/first.js
@@ -0,0 +1,4 @@
+let arr=[];
+arr.push(11);
+arr[8]=22;
+console.log(arr);
---
+++ array_js/second.js
@@ -0,0 +1,5 @@
+const age=[2010,2011,2014,2000]
+
+const currentage= age.map( Onage => 2020- Onage);
+
+console.log(currentage);
